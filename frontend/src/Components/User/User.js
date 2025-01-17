1d4a7a2e1e83bc7b42fc85dc4e641073ce3521fa
Himal <harithah2018@gmail.com>
2025-01-18 02:11:33 +0530

    Delete Done #12

--- frontend/src/Components/User/User.js
+++ frontend/src/Components/User/User.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function User(props) {
   const { _id, name, gmail, age, address } = props.user;
+
+  const history = useNavigate();
+  const deleteHandler = async () => {
+    await axios.delete(`http://localhost:5000/users/${_id}`)
+    .then (res => res.data)
+    .then(() => history("/"))
+    .then(() => history("/userdetails"));
+  }
   return (
     <div>
       <h1>User stats</h1>
@@ -13,7 +23,7 @@ function User(props) {
       <h1>Age:{age}</h1>
       <h1>Address:{address}</h1>
       <Link to ={`/userdetails/${_id}`}>Update</Link>
-      <button>Delete</button>
+      <button onClick={deleteHandler}>Delete</button>
     </div>
   );
 }
