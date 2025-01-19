a1889944413b9fa4e751ad73eedbee9dcefbf207
Himal <harithah2018@gmail.com>
2025-01-19 23:11:29 +0530

    #14 Searc bar completed with pdf error LOL

--- frontend/src/Components/UserDetails/Users.js
+++ frontend/src/Components/UserDetails/Users.js
@@ -1,44 +1,73 @@
-import React, { useState, useEffect,useRef} from "react";
-import Nav from '../Nav/nav'
+import React, { useState, useEffect, useRef } from "react";
+import Nav from "../Nav/nav";
 import axios from "axios";
 import User from "../User/User";
-import {useReactToPrint} from "react-to-print";
+import { useReactToPrint } from "react-to-print";
 
-const URL ="http://Localhost:5000/users";
+const URL = "http://Localhost:5000/users";
 
 const fetchHandler = async () => {
   return await axios.get(URL).then((res) => res.data);
 };
 
 function Users() {
-
   const [users, setUsers] = useState();
   useEffect(() => {
     fetchHandler().then((data) => setUsers(data.users));
-  },[])
+  }, []);
 
   const ComponentsRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => ComponentsRef.current,
     DocumentTitle: "Users Report",
-    onafterprint: ()=>alert("Users Report Successfully Download !"), 
+    onAfterPrint: () => alert("Users Report Successfully Download !"),
   });
+
+  const[searchQuery,setSearchQuery]= useState("");
+  const [noResult,setNoResult]=useState(false);
+
+  const handleSearch = () => {
+    fetchHandler().then((data) => {
+      const filteredUsers =data.users.filter((user) => 
+        Object.values(user).some((fields) =>
+        fields.toString().toLowerCase().includes(searchQuery.toLowerCase())
+        ))
+        setUsers(filteredUsers);
+        setNoResult(filteredUsers.length===0);
+      });
+  }
 
   return (
     <div>
-        <Nav />
+      <Nav />
       <h1>Users Details display page</h1>
-      <div ref={ComponentsRef}>
-        {users && users.map((user,i) => (
-          <div key={i}>
-            <User user={user} />
-          </div>
 
-         ))}
+      <input onChange ={(e)=>setSearchQuery(e.target.value)}
+      type="text"
+      name="search"
+      placeholder="Search User Details"></input>
+
+      <button onClick={handleSearch}>Search</button>
+
+      {noResult ? (
+        <div>
+          <p>No Users Found</p>
+        </div>
+
+      ) : (
+
+      <div ref={ComponentsRef}>
+        {users &&
+          users.map((user, i) => (
+            <div key={i}>
+              <User user={user} />
+            </div>
+          ))}
       </div>
-         <button onClick={handlePrint}>Download Report</button>
+      )}
+      <button onClick={handlePrint}>Download Report</button>
     </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
